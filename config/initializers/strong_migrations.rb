868b91a8aae86840494041f5058971007314bd36
Pablo Marti <pablo.marti.dev@gmail.com>
2020-08-12 19:00:38 -0600

    Fix offenses

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,5 +1,5 @@
 # Mark existing migrations as safe
-StrongMigrations.start_after = 20200813005722
+StrongMigrations.start_after = 20_200_813_005_722
 
 # Set timeouts for migrations
 # If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user
