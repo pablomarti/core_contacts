ed9114cfa10b983d12f841415a1f4c9a50896658
Pablo Marti <pablo.marti.dev@gmail.com>
2020-08-12 18:59:11 -0600

    Add gems

--- app/models/platform/country.rb
+++ app/models/platform/country.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+# Schema version: 20200813005722
+#
+# Table name: platform_countries
+#
+#  id         :uuid             not null, primary key
+#  code       :string           not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_platform_countries_on_code  (code) UNIQUE
+#
 class Platform::Country < ApplicationRecord
 end
--- test/models/platform/country_test.rb
+++ test/models/platform/country_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+# Schema version: 20200813005722
+#
+# Table name: platform_countries
+#
+#  id         :uuid             not null, primary key
+#  code       :string           not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_platform_countries_on_code  (code) UNIQUE
+#
 require 'test_helper'
 
 class Platform::CountryTest < ActiveSupport::TestCase
